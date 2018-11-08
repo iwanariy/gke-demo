226b1de3aac90ba01c49fd1d0107d7b393903a02
iwanariy <yuki.iwnr@gmail.com>
2018-11-09 00:29:08 +0900

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return('Hello Container World! {}'.format(os.uname()))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
